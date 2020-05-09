5ff73fe06904580ae2ab97879102fb10437d887c
Arthur Castro <arthurcastroes@gmail.com>
2020-05-09 11:13:56 -0300

    feat: cleaning

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
-
 import { Container } from './styles';
-
-import Logo from '../../assets/logo.svg';
 
 interface HeaderProps {
   size?: 'small' | 'large';
@@ -12,15 +8,7 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ size = 'large' }: HeaderProps) => (
   <Container size={size}>
-    <header>
-      <Link to="/">
-        <img src={Logo} alt="GoFinances" />
-      </Link>
-      <nav>
-        <Link to="/">Dashboard</Link>
-        <Link to="/import">Importar</Link>
-      </nav>
-    </header>
+    <header />
   </Container>
 );
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 
 import { Switch, Route } from 'react-router-dom';
 
-import Dashboard from '../pages/Dashboard';
-import Import from '../pages/Import';
+import SignIn from '../pages/SignIn';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" exact component={Dashboard} />
-    <Route path="/import" component={Import} />
+    <Route path="/" exact component={SignIn} />
   </Switch>
 );
 
