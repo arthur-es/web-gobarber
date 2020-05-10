9f95bae4f38875b69de829b9ef044eb175461755
Arthur Castro <arthurcastroes@gmail.com>
2020-05-10 16:40:10 -0300

    feat: auth routes

---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
+  align-items: stretch;
+
+  h1 {
+    color: white;
+  }
+`;
